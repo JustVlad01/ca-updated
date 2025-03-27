9c51c0724f85cdec4d1e6da11c211a2b3de7c33c
JustVlad01 <s00233992@atu.ie>
2025-03-27 14:10:18 +0000

    ssr removed from project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 import { authInterceptor } from './auth.interceptor';
@@ -11,7 +10,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }), // Helps with event coalescing in Angular
     provideRouter(routes), // Provide the routes for the app
     provideHttpClient(withInterceptors([authInterceptor])),
-    provideClientHydration(withEventReplay()), // Enable client-side hydration
     provideAnimationsAsync(), // Support animations asynchronously
   ]
 };
